a0bc103cd8617c45499ae9852a5e7790c8a7038b
yoneyan <github@yoneyan.net>
2020-09-12 04:05:10 +0900

    moved file

--- pkg/tool/token/token.go
+++ pkg/tool/token/token.go
@@ -1,11 +1,11 @@
-package tool
+package token
 
 import (
 	"github.com/google/uuid"
 	"log"
 )
 
-func GenerateToken(count int) (string, error) {
+func Generate(count int) (string, error) {
 	var token string
 	for i := 0; i < count; i++ {
 		tmpToken, err := uuid.NewRandom()
