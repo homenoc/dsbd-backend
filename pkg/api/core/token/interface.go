d344a58dd40a6e2aa2b5cd89c8e2810d98b4886d
yoneyan <github@yoneyan.net>
2020-09-12 23:18:24 +0900

    add token interface

---
+++ pkg/api/core/token/interface.go
@@ -0,0 +1,27 @@
+package token
+
+const (
+	ID                      = 0
+	UserTokenAndTmpToken    = 10
+	UserTokenAndAccessToken = 11
+)
+
+type Token struct {
+	ID          int    `json:"id"`
+	CreatedAt   int    `json:"created_at"`
+	UpdatedAt   int    `json:"updated_at"`
+	ExpiredAt   int    `json:"expired_at"`
+	DeletedAt   int    `json:"deleted_at"`
+	UID         int    `json:"uid"`
+	Status      int    `json:"status"` //0: before login 1: after login
+	UserToken   string `json:"user_token"`
+	TmpToken    string `json:"tmp_token"`
+	AccessToken string `json:"access_token"`
+	Debug       string `json:"debug"`
+}
+
+type Result struct {
+	Status bool    `json:"status"`
+	Error  string  `json:"error"`
+	Token  []Token `json:"token"`
+}
