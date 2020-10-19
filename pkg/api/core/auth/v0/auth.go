501517178ee4918370d48484bae8507d6186a7f3
yoneyan <github@yoneyan.net>
2020-10-20 02:57:08 +0900

    エラー処理の追加

--- pkg/api/core/auth/v0/auth.go
+++ pkg/api/core/auth/v0/auth.go
@@ -10,7 +10,6 @@ import (
 	dbToken "github.com/homenoc/dsbd-backend/pkg/store/token/v0"
 	dbUser "github.com/homenoc/dsbd-backend/pkg/store/user/v0"
 	"github.com/jinzhu/gorm"
-	"log"
 )
 
 func UserAuthentication(data token.Token) auth.UserResult {
@@ -21,12 +20,10 @@ func UserAuthentication(data token.Token) auth.UserResult {
 	if resultToken.Err != nil {
 		return auth.UserResult{Err: fmt.Errorf("db error")}
 	}
-	log.Println(resultToken)
 	resultUser := dbUser.Get(user.ID, &user.User{Model: gorm.Model{ID: resultToken.Token[0].UID}})
 	if resultUser.Err != nil {
 		return auth.UserResult{Err: fmt.Errorf("db error")}
 	}
-	log.Println(resultUser)
 	if 100 <= resultUser.User[0].Status {
 		return auth.UserResult{Err: fmt.Errorf("deleted this user")}
 	}
@@ -46,7 +43,10 @@ func GroupAuthentication(data token.Token) auth.GroupResult {
 		return auth.GroupResult{Err: fmt.Errorf("db error")}
 	}
 	if resultUser.User[0].Status == 0 || 100 <= resultUser.User[0].Status {
-		return auth.GroupResult{Err: fmt.Errorf("db error")}
+		return auth.GroupResult{Err: fmt.Errorf("user status error")}
+	}
+	if resultUser.User[0].GID == 0 {
+		return auth.GroupResult{Err: fmt.Errorf("no group")}
 	}
 	resultGroup := dbGroup.Get(group.ID, &group.Group{Model: gorm.Model{ID: resultUser.User[0].GID}})
 	if resultGroup.Err != nil {
