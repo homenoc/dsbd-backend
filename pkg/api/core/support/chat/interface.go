2b725be6fdb15471714536dbcfa86f5b28e29f2f
yoneyan <github@yoneyan.net>
2020-10-20 20:42:53 +0900

    NextIDの廃止、UserID、TicketIDの追加

--- pkg/api/core/support/chat/interface.go
+++ pkg/api/core/support/chat/interface.go
@@ -3,15 +3,18 @@ package chat
 import "github.com/jinzhu/gorm"
 
 const (
-	ID        = 0
-	UpdateAll = 110
+	ID           = 0
+	TicketID     = 1
+	UpdateNextID = 2
+	UpdateAll    = 110
 )
 
 type Chat struct {
 	gorm.Model
-	NextID uint   `json:"next_id"`
-	Admin  bool   `json:"admin"`
-	Data   string `json:"data"`
+	TicketID uint   `json:"ticket_id"`
+	UserID   uint   `json:"user_id"`
+	Admin    bool   `json:"admin"`
+	Data     string `json:"data"`
 }
 
 type ResultDatabase struct {
