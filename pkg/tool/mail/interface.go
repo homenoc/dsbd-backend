892facb7f791ddfa4e588d52c6cdc42c76bddc54
yoneyan <github@yoneyan.net>
2020-10-21 02:26:09 +0900

    メール処理の追加

--- pkg/tool/mail/interface.go
+++ pkg/tool/mail/interface.go
@@ -1,9 +1,7 @@
 package mail
 
-type mail struct {
-	user    string
-	pass    string
-	toMail  string
-	subject string
-	content string
+type Mail struct {
+	ToMail  string
+	Subject string
+	Content string
 }
