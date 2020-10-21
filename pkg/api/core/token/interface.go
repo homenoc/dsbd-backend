24a4988120b56876f027c4ee6a3d6f95fe425c4f
yoneyan <github@yoneyan.net>
2020-10-22 03:13:14 +0900

    AdminIDとconst値の修正

--- pkg/api/core/token/interface.go
+++ pkg/api/core/token/interface.go
@@ -12,13 +12,14 @@ const (
 	AdminToken              = 20
 	AddToken                = 100
 	UpdateToken             = 101
-	UpdateAll               = 101
+	UpdateAll               = 110
 )
 
 type Token struct {
 	gorm.Model
 	ExpiredAt   time.Time `json:"expired_at"`
 	UID         uint      `json:"uid"`
+	AdminID     uint      `json:"admin_id"`
 	Status      uint      `json:"status"` //0: initToken(30m) 1: 30m 2:6h 3: 12h 10: 30d 11:180d
 	Admin       bool      `json:"admin"`
 	UserToken   string    `json:"user_token"`
