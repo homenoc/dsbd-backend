bcdc24819214a1ee6c29f2587319dcc83c230e36
yoneyan <github@yoneyan.net>
2021-02-04 23:01:09 +0900

    [fix] 登録内容のチェック方法の修正

--- pkg/api/core/user/v0/check.go
+++ pkg/api/core/user/v0/check.go
@@ -3,6 +3,7 @@ package v0
 import (
 	"fmt"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/user"
+	"strings"
 )
 
 func check(input user.User) error {
@@ -10,5 +11,13 @@ func check(input user.User) error {
 	if input.Name == "" {
 		return fmt.Errorf("no data: name")
 	}
+
+	if !strings.Contains(input.Email, "@") {
+		return fmt.Errorf("wrong email address")
+	}
+	if input.Name == "" || input.NameEn == "" {
+		return fmt.Errorf("wrong name")
+	}
+
 	return nil
 }
