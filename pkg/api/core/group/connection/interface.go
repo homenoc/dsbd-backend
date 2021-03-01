021bcf88452c58856667e8a0fd965f8739abddaa
yoneyan <github@yoneyan.net>
2021-03-01 23:04:46 +0900

    [support] #31 [fix] #50 #54

--- pkg/api/core/group/connection/interface.go
+++ pkg/api/core/group/connection/interface.go
@@ -1,44 +1,52 @@
 package connection
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/homenoc/dsbd-backend/pkg/api/core/noc/gateway"
+	router "github.com/homenoc/dsbd-backend/pkg/api/core/noc/router"
+	"github.com/jinzhu/gorm"
+)
 
 const (
-	ID             = 0
-	OrgJa          = 1
-	Org            = 2
-	Email          = 3
-	GID            = 4
-	UpdateID       = 100
-	UpdateGID      = 101
-	UpdateUserInfo = 102
-	UpdateTechID   = 103
-	UpdateInfo     = 104
-	UpdateData     = 105
-	UpdateAll      = 110
+	ID              = 0
+	OrgJa           = 1
+	Org             = 2
+	Email           = 3
+	GID             = 4
+	SearchNewNumber = 5
+	UpdateID        = 100
+	UpdateGID       = 101
+	UpdateUserInfo  = 102
+	UpdateTechID    = 103
+	UpdateInfo      = 104
+	UpdateData      = 105
+	UpdateAll       = 110
 )
 
 type Connection struct {
 	gorm.Model
-	NetworkID     uint   `json:"network_id"`
-	GroupID       uint   `json:"group_id"`
-	ServiceID     string `json:"service_id"`
-	ServiceType   string `json:"service_type"`
-	ServiceYear   uint   `json:"service_year"`
-	ServiceNumber uint   `json:"service_number"`
-	UserID        uint   `json:"user_id"`
-	Service       string `json:"service"`
-	NTT           string `json:"ntt"`
-	NOC           string `json:"noc"`
-	NOCIP         string `json:"noc_ip"`
-	TermIP        string `json:"term_ip"`
-	Monitor       *bool  `json:"monitor"`
-	LinkV4Our     string `json:"link_v4_our"`
-	LinkV4Your    string `json:"link_v4_your"`
-	LinkV6Our     string `json:"link_v6_our"`
-	LinkV6Your    string `json:"link_v6_your"`
-	Fee           string `json:"fee"`
-	Open          *bool  `json:"open"`
-	Comment       string `json:"comment"`
+	NetworkID         uint            `json:"network_id"`
+	GroupID           uint            `json:"group_id"`
+	UserID            uint            `json:"user_id"`
+	RouterID          router.Router   `json:"router_id"`  //使用RouterのID
+	GatewayID         gateway.Gateway `json:"gateway_id"` //使用エンドポイントルータのID
+	ServiceID         string          `json:"service_id"` //廃止予定
+	ConnectionType    string          `json:"connection_type"`
+	ConnectionComment string          `json:"connection_comment"` // ServiceがETCの時や補足説明で必要
+	ConnectionNumber  uint            `json:"connection_number"`
+	Service           string          `json:"service"` //廃止予定
+	NTT               string          `json:"ntt"`
+	NOC               string          `json:"noc"`
+	NOCIP             string          `json:"noc_ip"` //廃止予定
+	TermIP            string          `json:"term_ip"`
+	Monitor           *bool           `json:"monitor"`
+	LinkV4Our         string          `json:"link_v4_our"`
+	LinkV4Your        string          `json:"link_v4_your"`
+	LinkV6Our         string          `json:"link_v6_our"`
+	LinkV6Your        string          `json:"link_v6_your"`
+	Fee               string          `json:"fee"`
+	Open              *bool           `json:"open"`
+	Lock              *bool           `json:"lock"`
+	Comment           string          `json:"comment"`
 }
 
 type Result struct {
--- pkg/api/core/group/connection/v0/connection.go
+++ pkg/api/core/group/connection/v0/connection.go
@@ -1,6 +1,7 @@
 package v0
 
 import (
+	"fmt"
 	"github.com/ashwanthkumar/slack-go-webhook"
 	"github.com/gin-gonic/gin"
 	auth "github.com/homenoc/dsbd-backend/pkg/api/core/auth/v0"
@@ -47,34 +48,59 @@ func Add(c *gin.Context) {
 		return
 	}
 
-	if err = check(input); err != nil {
+	if err = check(result.Group.ID, true, input); err != nil {
 		c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
 		return
 	}
 
+	resultConnection := dbConnection.Get(connection.GID, &connection.Connection{GroupID: result.Group.ID})
+	if resultConnection.Err != nil {
+		c.JSON(http.StatusBadRequest, common.Error{Error: resultConnection.Err.Error()})
+		return
+	}
+	var number uint = 1
+	for _, tmp := range resultConnection.Connection {
+		if tmp.ConnectionNumber >= 1 {
+			number = tmp.ConnectionNumber + 1
+		}
+	}
+
+	if number >= 999 {
+		c.JSON(http.StatusInternalServerError, common.Error{Error: "error: over number"})
+		return
+	}
+
 	_, err = dbConnection.Create(&connection.Connection{
-		GroupID: result.Group.ID,
-		UserID:  input.UserID,
-		Service: input.Service,
-		NTT:     input.NTT,
-		NOC:     input.NOC,
-		TermIP:  input.TermIP,
-		Monitor: input.Monitor,
-		Open:    &[]bool{false}[0]})
+		GroupID:          result.Group.ID,
+		UserID:           input.UserID,
+		ConnectionType:   input.ConnectionType,
+		ConnectionNumber: number,
+		NTT:              input.NTT,
+		NOC:              input.NOC,
+		TermIP:           input.TermIP,
+		Monitor:          input.Monitor,
+		Open:             &[]bool{false}[0],
+		Lock:             &[]bool{true}[0],
+	})
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
 		return
 	}
 
-	if err = dbGroup.Update(group.UpdateStatus, group.Group{Model: gorm.Model{ID: result.Group.ID},
-		Status: &[]uint{4}[0]}); err != nil {
+	if err = dbGroup.Update(group.UpdateStatus, group.Group{
+		Model:  gorm.Model{ID: result.Group.ID},
+		Status: &[]uint{4}[0],
+	}); err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
 		return
 	}
 
 	attachment := slack.Attachment{}
 	attachment.AddField(slack.Field{Title: "Title", Value: "接続情報登録"}).
-		AddField(slack.Field{Title: "GroupID", Value: strconv.Itoa(int(input.GroupID))})
+		AddField(slack.Field{Title: "申請者", Value: strconv.Itoa(int(result.User.ID)) + ":" + result.User.Name}).
+		AddField(slack.Field{Title: "GroupID", Value: strconv.Itoa(int(result.Group.ID)) + ":" + result.Group.Org}).
+		AddField(slack.Field{Title: "接続コード（新規発番）", Value: input.ConnectionType + fmt.Sprintf("%03d", number)}).
+		AddField(slack.Field{Title: "接続コード（補足情報）", Value: input.ConnectionComment})
 	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
 
 	c.JSON(http.StatusOK, group.Result{})
