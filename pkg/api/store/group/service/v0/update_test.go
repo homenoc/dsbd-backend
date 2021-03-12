0ee05f5d52c773ac1c3a294930f2a691fbe48eac
yoneyan <github@yoneyan.net>
2021-03-13 06:29:58 +0900

    [fix] #70 (DB処理の修正)

--- pkg/api/store/group/service/v0/update_test.go
+++ pkg/api/store/group/service/v0/update_test.go
@@ -1,6 +1,7 @@
 package v0
 
 import (
+	"github.com/homenoc/dsbd-backend/pkg/api/core"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/tool/config"
 	"testing"
 )
@@ -25,4 +26,55 @@ func TestDeleteJPNICTech(t *testing.T) {
 	if err = DeleteJPNICTech(1, 1); err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestAddJPNICTech(t *testing.T) {
+	err := config.GetConfig("/home/yonedayuto/go/src/github.com/homenoc/dsbd-backend/cmd/backend/con.json")
+	if err != nil {
+		t.Error(err)
+	}
+
+	data := core.Service{
+		GroupID:           0,
+		ServiceTemplateID: nil,
+		ServiceTemplate:   nil,
+		ServiceComment:    "",
+		ServiceNumber:     0,
+		Org:               "HomeNOC",
+		OrgEn:             "",
+		Postcode:          "",
+		Address:           "",
+		AddressEn:         "",
+		ASN:               0,
+		RouteV4:           "",
+		RouteV6:           "",
+		V4Name:            "",
+		V6Name:            "",
+		AveUpstream:       0,
+		MaxUpstream:       0,
+		AveDownstream:     0,
+		MaxDownstream:     0,
+		MaxBandWidthAS:    0,
+		IP:                nil,
+		Connections:       nil,
+		JPNICAdminID:      0,
+		JPNICAdmin: core.JPNICAdmin{
+			Org:   "HomeNOC",
+			OrgEn: "HomeNOC",
+		},
+		JPNICTech: []core.JPNICTech{
+			{
+				Org:   "HomeNOC",
+				OrgEn: "HomeNOC",
+			},
+		},
+		Open: nil,
+		Lock: nil,
+	}
+
+	result, err := Create(&data)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(result)
 }
