81542fa611b9a8e591b052fd277a1920a9cbc234
yoneyan <github@yoneyan.net>
2021-03-13 06:27:03 +0900

    [fix] #70 (templateの実装)

---
+++ pkg/api/core/template/interface.go
@@ -0,0 +1,24 @@
+package service
+
+import "github.com/homenoc/dsbd-backend/pkg/api/core"
+
+const (
+	ID        = 0
+	UpdateAll = 150
+)
+
+type Input struct {
+	Hidden  *bool  `json:"hidden"`
+	Name    string `json:"name"`
+	Comment string `json:"comment"`
+}
+
+type Result struct {
+	Services    []core.ServiceTemplate    `json:"services"`
+	Connections []core.ConnectionTemplate `json:"connections"`
+}
+
+type ResultDatabase struct {
+	Err      error
+	Services []core.ServiceTemplate
+}
---
+++ pkg/api/core/template/v0/admin.go
@@ -0,0 +1,35 @@
+package v0
+
+import (
+	"github.com/gin-gonic/gin"
+	auth "github.com/homenoc/dsbd-backend/pkg/api/core/auth/v0"
+	"github.com/homenoc/dsbd-backend/pkg/api/core/common"
+	template "github.com/homenoc/dsbd-backend/pkg/api/core/template"
+	dbConnectionTemplate "github.com/homenoc/dsbd-backend/pkg/api/store/template/connection/v0"
+	dbServiceTemplate "github.com/homenoc/dsbd-backend/pkg/api/store/template/service/v0"
+	"net/http"
+)
+
+func GetAdmin(c *gin.Context) {
+	resultAdmin := auth.AdminAuthentication(c.Request.Header.Get("ACCESS_TOKEN"))
+	if resultAdmin.Err != nil {
+		c.JSON(http.StatusUnauthorized, common.Error{Error: resultAdmin.Err.Error()})
+		return
+	}
+
+	resultService := dbServiceTemplate.GetAll()
+	if resultService.Err != nil {
+		c.JSON(http.StatusInternalServerError, common.Error{Error: resultService.Err.Error()})
+		return
+	}
+	resultConnection := dbConnectionTemplate.GetAll()
+	if resultConnection.Err != nil {
+		c.JSON(http.StatusInternalServerError, common.Error{Error: resultConnection.Err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, template.Result{
+		Services:    resultService.Services,
+		Connections: resultConnection.Connections,
+	})
+}
