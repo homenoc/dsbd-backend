473373182959cf431537e9e3566c0eb705759a74
yoneyan <github@yoneyan.net>
2021-03-13 06:25:44 +0900

    [fix] #70 (service側の実装)

--- pkg/api/core/group/service/v0/admin.go
+++ pkg/api/core/group/service/v0/admin.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 	"github.com/ashwanthkumar/slack-go-webhook"
 	"github.com/gin-gonic/gin"
+	"github.com/homenoc/dsbd-backend/pkg/api/core"
 	auth "github.com/homenoc/dsbd-backend/pkg/api/core/auth/v0"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/common"
-	"github.com/homenoc/dsbd-backend/pkg/api/core/group/network"
+	"github.com/homenoc/dsbd-backend/pkg/api/core/group/service"
+	serviceTemplate "github.com/homenoc/dsbd-backend/pkg/api/core/template/service"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/tool/notification"
-	"github.com/homenoc/dsbd-backend/pkg/api/core/user"
-	dbNetwork "github.com/homenoc/dsbd-backend/pkg/api/store/group/network/v0"
-	dbUser "github.com/homenoc/dsbd-backend/pkg/api/store/user/v0"
+	dbService "github.com/homenoc/dsbd-backend/pkg/api/store/group/service/v0"
+	dbServiceTemplate "github.com/homenoc/dsbd-backend/pkg/api/store/template/service/v0"
 	"github.com/jinzhu/gorm"
 	"log"
 	"net/http"
@@ -31,7 +32,7 @@ func AddAdmin(c *gin.Context) {
 		return
 	}
 
-	var input network.Input
+	var input service.Input
 
 	resultAdmin := auth.AdminAuthentication(c.Request.Header.Get("ACCESS_TOKEN"))
 	if resultAdmin.Err != nil {
@@ -46,54 +47,83 @@ func AddAdmin(c *gin.Context) {
 	}
 
 	// check json
-	if err = checkAdmin(input); err != nil {
+	if err = check(input); err != nil {
 		c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
 		return
 	}
 
 	log.Println(input)
 
-	var grpIP *[]network.IP
+	var grpIP []core.IP = nil
 
-	if !(input.NetworkType == "ET00") {
-		grpIP, err = ipProcess(input)
+	resultServiceTemplate := dbServiceTemplate.Get(serviceTemplate.ID, &core.ServiceTemplate{Model: gorm.Model{ID: input.ServiceTemplateID}})
+	if resultServiceTemplate.Err != nil {
+		c.JSON(http.StatusBadRequest, common.Error{Error: resultServiceTemplate.Err.Error()})
+		return
+	}
+
+	if *resultServiceTemplate.Services[0].NeedJPNIC {
+		if err = checkJPNIC(input); err != nil {
+			c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
+			return
+		}
+
+		if err = checkJPNICAdminUser(input.JPNICAdmin); err != nil {
+			c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
+			return
+		}
+
+		if len(input.JPNICTech) == 0 || len(input.JPNICTech) > 2 {
+			c.JSON(http.StatusBadRequest, common.Error{Error: "error: user tech count"})
+			return
+		}
+
+		for _, tmp := range input.JPNICTech {
+			if err = checkJPNICTechUser(tmp); err != nil {
+				c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
+				return
+			}
+		}
+
+		grpIP, err = ipProcess(true, input.IP)
 		if err != nil {
-			log.Println(err)
-			c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
-			return
-		}
-	} else {
-		grpIP = nil
-	}
-
-	jh := adminTechHandler{
-		admin:       input.AdminID,
-		tech:        input.TechID,
-		groupID:     uint(id),
-		resultAdmin: nil,
-		resultTech:  nil,
-	}
-
-	// 2000,3S00,3B00の場合
-	if input.NetworkType == "2000" || input.NetworkType == "3S00" ||
-		input.NetworkType == "3B00" || input.NetworkType == "CL20" || input.NetworkType == "CL3S" ||
-		input.NetworkType == "CL3B" || input.NetworkType == "IP3B" {
-		if err = jh.AdminTechProcess(); err != nil {
 			c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
 			return
 		}
 	}
 
-	resultNetwork := dbNetwork.Get(network.SearchNewNumber, &network.Network{GroupID: uint(id)})
+	if *resultServiceTemplate.Services[0].NeedComment && input.ServiceComment == "" {
+		c.JSON(http.StatusBadRequest, common.Error{Error: "no data: comment"})
+		return
+	}
+
+	if *resultServiceTemplate.Services[0].NeedGlobalAS {
+		if input.ASN == 0 {
+			c.JSON(http.StatusBadRequest, common.Error{Error: "no data: ASN"})
+			return
+		}
+
+		grpIP, err = ipProcess(false, input.IP)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
+			return
+		}
+	}
+
+	if *resultServiceTemplate.Services[0].NeedRoute && input.RouteV4 == "" && input.RouteV6 == "" {
+		c.JSON(http.StatusBadRequest, common.Error{Error: "no data: Route Information"})
+		return
+	}
+
+	resultNetwork := dbService.Get(service.SearchNewNumber, &core.Service{GroupID: uint(id)})
 	if resultNetwork.Err != nil {
 		c.JSON(http.StatusBadRequest, common.Error{Error: resultNetwork.Err.Error()})
 		return
 	}
-
 	var number uint = 1
-	for _, tmp := range resultNetwork.Network {
-		if tmp.NetworkNumber >= 1 {
-			number = tmp.NetworkNumber + 1
+	for _, tmp := range resultNetwork.Service {
+		if tmp.ServiceNumber >= 1 {
+			number = tmp.ServiceNumber + 1
 		}
 	}
 
@@ -103,24 +133,28 @@ func AddAdmin(c *gin.Context) {
 	}
 
 	//db create for network
-	_, err = dbNetwork.Create(&network.Network{
-		GroupID:        uint(id),
-		NetworkType:    input.NetworkType,
-		NetworkComment: input.NetworkComment,
-		NetworkNumber:  number,
-		Org:            input.Org,
-		OrgEn:          input.OrgEn,
-		Postcode:       input.Postcode,
-		Address:        input.Address,
-		AddressEn:      input.AddressEn,
-		RouteV4:        input.RouteV4,
-		RouteV6:        input.RouteV6,
-		ASN:            input.ASN,
-		Open:           &[]bool{false}[0],
-		IP:             *grpIP,
-		Admin:          *jh.resultAdmin,
-		Tech:           *jh.resultTech,
-		Lock:           &[]bool{true}[0],
+	_, err = dbService.Create(&core.Service{
+		GroupID:           uint(id),
+		ServiceTemplateID: &input.ServiceTemplateID,
+		ServiceComment:    input.ServiceComment,
+		ServiceNumber:     number,
+		Org:               input.Org,
+		OrgEn:             input.OrgEn,
+		Postcode:          input.Postcode,
+		Address:           input.Address,
+		AddressEn:         input.AddressEn,
+		RouteV4:           input.RouteV4,
+		RouteV6:           input.RouteV6,
+		AveUpstream:       input.AveUpstream,
+		MaxUpstream:       input.MaxUpstream,
+		AveDownstream:     input.AveDownstream,
+		MaxDownstream:     input.MaxDownstream,
+		ASN:               input.ASN,
+		IP:                grpIP,
+		JPNICAdmin:        input.JPNICAdmin,
+		JPNICTech:         input.JPNICTech,
+		Open:              &[]bool{false}[0],
+		Lock:              &[]bool{true}[0],
 	})
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
@@ -131,11 +165,11 @@ func AddAdmin(c *gin.Context) {
 	attachment.AddField(slack.Field{Title: "Title", Value: "ネットワーク情報登録(管理者実行)"}).
 		AddField(slack.Field{Title: "申請者", Value: "管理者"}).
 		AddField(slack.Field{Title: "GroupID", Value: strconv.Itoa(id)}).
-		AddField(slack.Field{Title: "サービスコード（新規発番）", Value: input.NetworkType + fmt.Sprintf("%03d", number)}).
-		AddField(slack.Field{Title: "サービスコード（補足情報）", Value: input.NetworkComment})
+		AddField(slack.Field{Title: "サービスコード（新規発番）", Value: resultServiceTemplate.Services[0].Type + fmt.Sprintf("%03d", number)}).
+		AddField(slack.Field{Title: "サービスコード（補足情報）", Value: input.ServiceComment})
 	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
 
-	c.JSON(http.StatusOK, network.Result{})
+	c.JSON(http.StatusOK, service.Result{})
 }
 
 func DeleteAdmin(c *gin.Context) {
@@ -151,15 +185,15 @@ func DeleteAdmin(c *gin.Context) {
 		return
 	}
 
-	if err := dbNetwork.Delete(&network.Network{Model: gorm.Model{ID: uint(id)}}); err != nil {
+	if err := dbService.Delete(&core.Service{Model: gorm.Model{ID: uint(id)}}); err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, network.Result{})
+	c.JSON(http.StatusOK, service.Result{})
 }
 
 func UpdateAdmin(c *gin.Context) {
-	var input network.Network
+	var input core.Service
 
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -180,17 +214,17 @@ func UpdateAdmin(c *gin.Context) {
 		return
 	}
 
-	result := dbNetwork.Get(network.ID, &network.Network{Model: gorm.Model{ID: uint(id)}})
+	result := dbService.Get(service.ID, &core.Service{Model: gorm.Model{ID: uint(id)}})
 	if result.Err != nil {
 		c.JSON(http.StatusBadRequest, common.Error{Error: resultAdmin.Err.Error()})
 		return
 	}
 
-	if err := dbNetwork.Update(network.UpdateAll, replaceAdminNetwork(result.Network[0], input)); err != nil {
+	if err = dbService.Update(service.UpdateAll, replaceAdminService(result.Service[0], input)); err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, network.Result{})
+	c.JSON(http.StatusOK, service.Result{})
 }
 
 func GetAdmin(c *gin.Context) {
@@ -205,18 +239,13 @@ func GetAdmin(c *gin.Context) {
 		return
 	}
 
-	result := dbNetwork.Get(network.ID, &network.Network{Model: gorm.Model{ID: uint(id)}})
+	result := dbService.Get(service.ID, &core.Service{Model: gorm.Model{ID: uint(id)}})
 	if result.Err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: result.Err.Error()})
 		return
 	}
 
-	resultUser := dbUser.Get(user.GID, &user.User{GroupID: result.Network[0].GroupID})
-	if resultUser.Err != nil {
-		c.JSON(http.StatusInternalServerError, common.Error{Error: resultUser.Err.Error()})
-		return
-	}
-	c.JSON(http.StatusOK, network.Result{User: resultUser.User, Network: result.Network})
+	c.JSON(http.StatusOK, service.Result{Service: result.Service})
 }
 
 func Get(c *gin.Context) {
@@ -226,9 +255,9 @@ func Get(c *gin.Context) {
 		return
 	}
 
-	if result := dbNetwork.GetAll(); result.Err != nil {
+	if result := dbService.GetAll(); result.Err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: result.Err.Error()})
 	} else {
-		c.JSON(http.StatusOK, network.Result{Network: result.Network})
+		c.JSON(http.StatusOK, service.Result{Service: result.Service})
 	}
 }
--- pkg/api/core/group/service/v0/ip.go
+++ pkg/api/core/group/service/v0/ip.go
@@ -1,18 +1,15 @@
 package v0
 
 import (
-	network "github.com/homenoc/dsbd-backend/pkg/api/core/group/network"
+	"github.com/homenoc/dsbd-backend/pkg/api/core"
+	"github.com/homenoc/dsbd-backend/pkg/api/core/group/service"
 	"time"
 )
 
-func ipProcess(input network.Input) (*[]network.IP, error) {
-	var net []network.IP
+func ipProcess(restrict bool, input []service.IPInput) ([]core.IP, error) {
+	var net []core.IP
 
-	for _, tmpIP := range input.IP {
-		restrict := true
-		if input.NetworkType == "IP3B" {
-			restrict = false
-		}
+	for _, tmpIP := range input {
 		if err := ipCheck(restrict, tmpIP); err != nil {
 			return nil, err
 		}
@@ -23,7 +20,8 @@ func ipProcess(input network.Input) (*[]network.IP, error) {
 			tmpEndDate, _ := time.Parse("2006-01-02", *tmpIP.EndDate)
 			endDate = &tmpEndDate
 		}
-		net = append(net, network.IP{
+
+		net = append(net, core.IP{
 			Version:   tmpIP.Version,
 			Name:      tmpIP.Name,
 			IP:        tmpIP.IP,
@@ -34,5 +32,5 @@ func ipProcess(input network.Input) (*[]network.IP, error) {
 			Open:      &[]bool{false}[0],
 		})
 	}
-	return &net, nil
+	return net, nil
 }
