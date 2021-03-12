c32c80112ff56251ce171d38e9f00ddf5098cf7b
yoneyan <github@yoneyan.net>
2021-03-13 06:29:20 +0900

    [fix] #70 (DB初期化システムの実装)

--- pkg/backend/cmd/init.go
+++ pkg/backend/cmd/init.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/homenoc/dsbd-backend/pkg/api/core/tool/config"
+	initFunction "github.com/homenoc/dsbd-backend/pkg/api/core/tool/init"
 	"github.com/homenoc/dsbd-backend/pkg/api/store"
 	"github.com/spf13/cobra"
 	"log"
@@ -9,8 +10,14 @@ import (
 
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "init database",
+	Short: "init command",
 	Long:  ``,
+}
+
+var initDatabaseCmd = &cobra.Command{
+	Use:   "database",
+	Short: "init database",
+	Long:  `init database`,
 	Run: func(cmd *cobra.Command, args []string) {
 		confPath, err := cmd.Flags().GetString("config")
 		if err != nil {
@@ -26,6 +33,37 @@ var initCmd = &cobra.Command{
 	},
 }
 
+var initRegisterCmd = &cobra.Command{
+	Use:   "register",
+	Short: "register database table",
+	Long:  `register database table`,
+	Run: func(cmd *cobra.Command, args []string) {
+		confPath, err := cmd.Flags().GetString("config")
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+		template, err := cmd.Flags().GetString("template")
+		if err != nil {
+			log.Fatalf("could not greet: %v", err)
+		}
+
+		if config.GetConfig(confPath) != nil {
+			log.Fatalf("error config process |%v", err)
+		}
+
+		if err = initFunction.RegisterTemplateConfig(template); err != nil {
+			log.Fatalf("error config process |%v", err)
+		}
+
+		//api.AdminRestAPI()
+		log.Println("end")
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(initCmd)
+	initCmd.AddCommand(initDatabaseCmd)
+	initCmd.AddCommand(initRegisterCmd)
+	initCmd.PersistentFlags().StringP("config", "c", "", "config path")
+	initCmd.PersistentFlags().StringP("template", "t", "", "template path")
 }
