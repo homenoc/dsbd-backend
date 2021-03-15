4e14e87a8a86c5b7cadcffb4d34ae3e1a743262f
yoneyan <github@yoneyan.net>
2021-03-16 05:59:49 +0900

    [refactoring] #48

--- pkg/api/core/group/service/v0/update.go
+++ pkg/api/core/group/service/v0/update.go
@@ -15,9 +15,9 @@ func replaceService(replace, input core.Service) core.Service {
 		replace.OrgEn = input.OrgEn
 	}
 
-	//Postcode
-	if input.Postcode != "" {
-		replace.Postcode = input.Postcode
+	//PostCode
+	if input.PostCode != "" {
+		replace.PostCode = input.PostCode
 	}
 
 	//Address
@@ -68,76 +68,6 @@ func replaceService(replace, input core.Service) core.Service {
 	//if input.Date != "" {
 	//	replace.Date = input.Date
 	//}
-
-	return replace
-}
-
-func replaceAdminService(replace, input core.Service) core.Service {
-	//Org
-	if input.Org != "" {
-		replace.Org = input.Org
-	}
-
-	//Org (English)
-	if input.OrgEn != "" {
-		replace.OrgEn = input.OrgEn
-	}
-
-	//Postcode
-	if input.Postcode != "" {
-		replace.Postcode = input.Postcode
-	}
-
-	//Address
-	if input.Address != "" {
-		replace.Address = input.Address
-	}
-
-	//Address(English)
-	if input.AddressEn != "" {
-		replace.AddressEn = input.AddressEn
-	}
-
-	//Route(V4)
-	if input.RouteV4 != "" {
-		replace.RouteV4 = input.RouteV4
-	}
-
-	//Route(V4)
-	if input.RouteV6 != "" {
-		replace.RouteV6 = input.RouteV6
-	}
-
-	////V4
-	//if input.V4 != "" {
-	//	replace.V4 = input.V4
-	//}
-	//
-	////V6
-	//if input.V6 != "" {
-	//	replace.V6 = input.V6
-	//}
-	//
-	////V4Name
-	//if input.V4Name != "" {
-	//	replace.V4Name = input.V4Name
-	//}
-	//
-	////V6Name
-	//if input.V6Name != "" {
-	//	replace.V6Name = input.V6Name
-	//}
-	////Date
-	//if input.Date != "" {
-	//	replace.Date = input.Date
-	//}
-
-	// bool
-	//Lock
-	replace.Lock = input.Lock
-
-	//Open
-	replace.Open = input.Open
 
 	return replace
 }
