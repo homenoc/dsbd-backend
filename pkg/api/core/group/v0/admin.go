ed9de6cecac6245e764484c6dc41f1bb9f84cb69
yoneyan <github@yoneyan.net>
2021-03-16 03:10:53 +0900

    [fix] #55 (グループ部分の通知処理を強化)

--- pkg/api/core/group/v0/admin.go
+++ pkg/api/core/group/v0/admin.go
@@ -1,13 +1,12 @@
 package v0
 
 import (
-	"github.com/ashwanthkumar/slack-go-webhook"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/homenoc/dsbd-backend/pkg/api/core"
 	auth "github.com/homenoc/dsbd-backend/pkg/api/core/auth/v0"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/common"
 	"github.com/homenoc/dsbd-backend/pkg/api/core/group"
-	"github.com/homenoc/dsbd-backend/pkg/api/core/tool/notification"
 	dbGroup "github.com/homenoc/dsbd-backend/pkg/api/store/group/v0"
 	"github.com/jinzhu/gorm"
 	"log"
@@ -59,6 +58,19 @@ func DeleteAdmin(c *gin.Context) {
 }
 
 func UpdateAdmin(c *gin.Context) {
+	// ID取得
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
+		return
+	}
+
+	// serviceIDが0の時エラー処理
+	if id == 0 {
+		c.JSON(http.StatusBadRequest, common.Error{Error: fmt.Sprintf("This id is wrong... ")})
+		return
+	}
+
 	var input core.Group
 
 	resultAdmin := auth.AdminAuthentication(c.Request.Header.Get("ACCESS_TOKEN"))
@@ -67,48 +79,24 @@ func UpdateAdmin(c *gin.Context) {
 		return
 	}
 
-	err := c.BindJSON(&input)
+	err = c.BindJSON(&input)
 	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusBadRequest, common.Error{Error: err.Error()})
 		return
 	}
 
-	tmp := dbGroup.Get(group.ID, &core.Group{Model: gorm.Model{ID: input.ID}})
+	tmp := dbGroup.Get(group.ID, &core.Group{Model: gorm.Model{ID: uint(id)}})
 	if tmp.Err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: tmp.Err.Error()})
 		return
 	}
 
-	// 審査ステータスのSlack通知
-	if *tmp.Group[0].Pass != *input.Pass {
-		attachment := slack.Attachment{}
-		if *input.Pass {
-			attachment.AddField(slack.Field{Title: "Title", Value: "ステータス変更"}).
-				AddField(slack.Field{Title: "申請者", Value: "管理者"}).
-				AddField(slack.Field{Title: "Group", Value: strconv.Itoa(int(tmp.Group[0].ID)) + ":" + tmp.Group[0].Org}).
-				AddField(slack.Field{Title: "現在ステータス情報", Value: "審査中"}).
-				AddField(slack.Field{Title: "ステータス履歴", Value: "[審査合格] =>[審査中] "})
-			notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
+	noticeSlackAdmin(tmp.Group[0], input)
 
-		} else if !(*input.Pass) {
-			attachment.AddField(slack.Field{Title: "Title", Value: "ステータス変更"}).
-				AddField(slack.Field{Title: "申請者", Value: "管理者"}).
-				AddField(slack.Field{Title: "Group", Value: strconv.Itoa(int(tmp.Group[0].ID)) + ":" + tmp.Group[0].Org}).
-				AddField(slack.Field{Title: "現在ステータス情報", Value: "審査合格処理終了"}).
-				AddField(slack.Field{Title: "ステータス履歴", Value: "[審査中] =>[審査合格処理]"})
-			notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
+	input.ID = uint(id)
 
-		}
-	}
-
-	replace, err := updateAdminGroup(input, tmp.Group[0])
-	if err != nil {
-		c.JSON(http.StatusBadRequest, common.Error{Error: "error: this email is already registered"})
-		return
-	}
-
-	if err = dbGroup.Update(group.UpdateAll, replace); err != nil {
+	if err = dbGroup.Update(group.UpdateAll, input); err != nil {
 		c.JSON(http.StatusInternalServerError, common.Error{Error: err.Error()})
 		return
 	}
