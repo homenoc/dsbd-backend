d83a1c39ae10266d2c3b6b1219b54ded4a158242
yoneyan <github@yoneyan.net>
2021-03-20 16:40:32 +0900

    [delete] 不要になったサービス処理の削除

--- pkg/api/core/service/interface.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "github.com/homenoc/dsbd-backend/pkg/api/core/tool/config"
-
-type Result struct {
-	Network    []config.Network    `json:"network"`
-	Connection []config.Connection `json:"connection"`
-}
