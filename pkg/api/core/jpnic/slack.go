fb1be9aeed19c14e35c26aa4fd0bda34941f9777
yoneyan <github@yoneyan.net>
2021-08-27 03:02:53 +0900

    [support] --beta support-- supported jpnic registration.

---
+++ pkg/api/core/jpnic/slack.go
@@ -0,0 +1,32 @@
+package jpnic
+
+import (
+	"github.com/ashwanthkumar/slack-go-webhook"
+	"github.com/homenoc/dsbd-backend/pkg/api/core/tool/notification"
+	jpnicTransaction "github.com/homenoc/jpnic"
+)
+
+func success(data jpnicTransaction.Result) {
+	attachment := slack.Attachment{}
+
+	attachment.Text = &[]string{"JPNIC登録(手動実行)"}[0]
+	attachment.AddField(slack.Field{Title: "申請者", Value: "管理者"}).
+		AddField(slack.Field{Title: "RecepNo", Value: data.RecepNo}).
+		AddField(slack.Field{Title: "AdmJPNICHdl（新規発番）", Value: data.AdmJPNICHdl}).
+		AddField(slack.Field{Title: "Tech1JPNICHdl（補足情報）", Value: data.Tech1JPNICHdl}).
+		AddField(slack.Field{Title: "Tech2JPNICHdl（補足情報）", Value: data.Tech2JPNICHdl})
+
+	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
+}
+
+func bad(data jpnicTransaction.Result) {
+	attachment := slack.Attachment{}
+
+	attachment.Text = &[]string{"[失敗]JPNIC登録(手動実行)"}[0]
+	attachment.AddField(slack.Field{Title: "申請者", Value: "管理者"}).
+		AddField(slack.Field{Title: "RecepNo", Value: data.RecepNo}).
+		AddField(slack.Field{Title: "AdmJPNICHdl（新規発番）", Value: data.AdmJPNICHdl}).
+		AddField(slack.Field{Title: "Tech1JPNICHdl（補足情報）", Value: data.Tech1JPNICHdl}).
+		AddField(slack.Field{Title: "Tech2JPNICHdl（補足情報）", Value: data.Tech2JPNICHdl})
+	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: false})
+}
