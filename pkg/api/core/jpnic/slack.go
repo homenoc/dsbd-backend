c9e928ef769a8f5d036802d3fea92db01a7ebba5
yoneyan <github@yoneyan.net>
2021-09-04 21:10:29 +0900

    [support] Supported return address.

--- pkg/api/core/jpnic/slack.go
+++ pkg/api/core/jpnic/slack.go
@@ -6,7 +6,7 @@ import (
 	jpnicTransaction "github.com/homenoc/jpnic"
 )
 
-func success(data jpnicTransaction.Result) {
+func registrationSuccess(data jpnicTransaction.Result) {
 	attachment := slack.Attachment{}
 
 	attachment.Text = &[]string{"JPNIC登録(手動実行)"}[0]
@@ -19,7 +19,7 @@ func success(data jpnicTransaction.Result) {
 	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
 }
 
-func bad(data jpnicTransaction.Result) {
+func registrationBad(data jpnicTransaction.Result) {
 	attachment := slack.Attachment{}
 
 	var errStr string
@@ -31,5 +31,49 @@ func bad(data jpnicTransaction.Result) {
 	attachment.AddField(slack.Field{Title: "申請者", Value: "管理者"}).
 		AddField(slack.Field{Title: "Error", Value: data.Err.Error()}).
 		AddField(slack.Field{Title: "Error(詳細)", Value: errStr})
+	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: false})
+}
+
+func returnSuccess(input ReturnInput, data string) {
+	attachment := slack.Attachment{}
+
+	var addresses string
+	for _, tmp := range input.Address {
+		addresses = addresses + ", " + tmp
+	}
+	returnDate := "[未指定]2ヶ月後"
+	if input.ReturnDate != "" {
+		returnDate = input.ReturnDate
+	}
+
+	attachment.Text = &[]string{"JPNICアドレス返却"}[0]
+	attachment.AddField(slack.Field{Title: "申請者", Value: "管理者"}).
+		AddField(slack.Field{Title: "申請アドレス", Value: addresses}).
+		AddField(slack.Field{Title: "NetworkName", Value: input.NetworkName}).
+		AddField(slack.Field{Title: "返却日", Value: returnDate}).
+		AddField(slack.Field{Title: "Result", Value: data})
+
+	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: true})
+}
+
+func returnBad(input ReturnInput, data string) {
+	attachment := slack.Attachment{}
+
+	var addresses string
+	for _, tmp := range input.Address {
+		addresses = addresses + ", " + tmp
+	}
+	returnDate := "[未指定]2ヶ月後"
+	if input.ReturnDate != "" {
+		returnDate = input.ReturnDate
+	}
+
+	attachment.Text = &[]string{"JPNICアドレス返却"}[0]
+	attachment.AddField(slack.Field{Title: "申請者", Value: "管理者"}).
+		AddField(slack.Field{Title: "申請アドレス", Value: addresses}).
+		AddField(slack.Field{Title: "NetworkName", Value: input.NetworkName}).
+		AddField(slack.Field{Title: "返却日", Value: returnDate}).
+		AddField(slack.Field{Title: "Result", Value: data})
+
 	notification.SendSlack(notification.Slack{Attachment: attachment, ID: "main", Status: false})
 }
