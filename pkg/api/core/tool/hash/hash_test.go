47955da1407dcdc891e61b1b76c7d7668b4c86c9
yoneyan <git@yoneyan.net>
2021-10-30 04:35:38 +0900

    oops...

--- pkg/api/core/tool/hash/hash_test.go
+++ pkg/api/core/tool/hash/hash_test.go
@@ -5,11 +5,11 @@ import "testing"
 func TestHashProcess(t *testing.T) {
 	baseData := "test1test2test3"
 	hash := Generate(baseData)
-	t.Logf("Hash: %s", hash)
+	t.Logf("Hash: %s\n", hash)
 
 	if Verify(baseData, hash) {
-		t.Logf("OK")
+		t.Logf("OK\n")
 	} else {
-		t.Logf("NG")
+		t.Logf("NG\n")
 	}
 }
