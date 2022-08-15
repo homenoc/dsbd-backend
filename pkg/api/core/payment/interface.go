aea73f19481d0ccb111bb4ef4c30c20cf9a72906
yoneyan <contact@yoneyan.net>
2022-08-15 22:08:11 +0900

    [fix] #144

--- pkg/api/core/payment/interface.go
+++ pkg/api/core/payment/interface.go
@@ -1,24 +1,11 @@
 package payment
 
-import "github.com/homenoc/dsbd-backend/pkg/api/core"
-
-const (
-	ID              = 0
-	PaymentIntentID = 1
-	UpdatePaid      = 140
-	UpdateAll       = 150
-)
-
 type Input struct {
 	Plan string `json:"plan"` // yearly, monthly
 }
 
 type ChangeCardPaymentInit struct {
 	PaymentMethodID string `json:"payment_method_id"`
-}
-
-type ResultByAdmin struct {
-	Payment []core.Payment `json:"payment"`
 }
 
 type ResultByUser struct {
